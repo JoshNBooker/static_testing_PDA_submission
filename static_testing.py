97c29edee06a176f95c8a11f620c67e78d441e45
Josh Booker <josh.booker2@gmail.com>
2023-07-25 19:00:49 +0100

    answers to static testing PDA assignment

---
+++ static_testing.py
@@ -0,0 +1,23 @@
+
+class CardGame:
+
+
+    def check_for_ace(self, card):
+        if card.value = 1:      # operator should be ==
+            return True
+        else                    # else needs a colon
+            return False
+
+
+    dif highest_card(self, card1 card2): #dif should be def, no comma between card1 and card2
+    if card1.value > card2.value: #line should be indented
+        return card
+    else:
+        return card2
+
+
+def cards_total(self, cards): # line should be indented
+total                         #this doesn't need to be here at all
+for card in cards:            # no need for this to be on line below
+    total += card.value       
+    return "You have a total of" + total 
